d8d1d8e5d48b5daa8fd5f6068f0b09a7fa69a96d
Megumi-cpu <20241391@lamolina.edu.pe>
2025-10-27 19:06:38 -0500

    se creo la clase variable estadistica

---
+++ codigo.py
@@ -0,0 +1,15 @@
+# ----------------------------- #
+# 🧩 CLASE PADRE
+# ----------------------------- #
+class VariableEstadistica:
+    def __init__(self, df, columna):
+        self.df = df
+        self.columna = columna
+        # elimina valores nulos y los guarda como lista
+        self.datos = df[columna].dropna().tolist()
+
+    def cantidad(self):
+        return len(self.datos)
+
+    def mostrar_datos(self, n=10):
+        print(self.datos[:n])
